ab737e19010cd725d142cf43785e5c7bf503ca9d
John Reilly <johnny_reilly@hotmail.com>
2015-11-27 12:10:46 +0000

    Now not dependent upon current and has 32 64 version support

--- Wget/tools/ChocolateyInstall.ps1
+++ Wget/tools/ChocolateyInstall.ps1
@@ -1,3 +1,9 @@
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$wgetPath = Join-Path $scriptPath 'wget.exe'
-Get-ChocolateyWebFile 'Wget' "$wgetPath" 'https://eternallybored.org/misc/wget/current/wget.exe'
+$packageName = "Wget"
+$url = "https://eternallybored.org/misc/wget/releases/wget-1.17-win32.zip"
+$url64 = "https://eternallybored.org/misc/wget/releases/wget-1.17-win64.zip"
+$unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# as per: https://www.gnu.org/prep/ftp.html
+# which point to: http://wget.addictivecode.org/FrequentlyAskedQuestions?action=show&redirect=Faq#download
+# I'm getting the binaries from: https://eternallybored.org/misc/wget/
+Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
